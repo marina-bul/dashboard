7978b3d7685d40d034df825aa4c0f36f66d59b92
marina-bul <bragina_marina@mail.ru>
2025-04-11 15:49:28 +0300

    fix: Change config

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,7 @@ export default defineConfig({
       shared: path.resolve(__dirname, 'src/shared'),
       types: path.resolve(__dirname, 'src/types'),
     },
-  }
+  },
+
+  base: '/dashboard/',
 })
